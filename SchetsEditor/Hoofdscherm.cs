b38b9c8dec44f940824928be096590c295da255f
NSavenije <noud.savenije@hotmail.com>
2014-10-22 13:27:33 +0200

    schetseditor
    versie 0.0

---
+++ SchetsEditor/Hoofdscherm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SchetsEditor
+{
+    public class Hoofdscherm : Form
+    {
+        MenuStrip menuStrip;
+
+        public Hoofdscherm()
+        {   this.ClientSize = new Size(800, 600);
+            menuStrip = new MenuStrip();
+            this.Controls.Add(menuStrip);
+            this.maakFileMenu();
+            this.maakHelpMenu();
+            this.Text = "Schets editor";
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = menuStrip;
+        }
+        private void maakFileMenu()
+        {   ToolStripDropDownItem menu;
+            menu = new ToolStripMenuItem("File");
+            menu.DropDownItems.Add("Nieuw", null, this.nieuw);
+            menu.DropDownItems.Add("Opslaan", null, this.opslaan);
+            menu.DropDownItems.Add("Laden", null, this.laden);
+            menu.DropDownItems.Add("Afsluiten", null, this.afsluiten);
+            menuStrip.Items.Add(menu);
+        }
+        private void maakHelpMenu()
+        {   ToolStripDropDownItem menu;
+            menu = new ToolStripMenuItem("Help");
+            menu.DropDownItems.Add("Over \"Schets\"", null, this.about);
+            menuStrip.Items.Add(menu);
+        }
+        private void about(object o, EventArgs ea)
+        {   MessageBox.Show("Schets versie 1.0\n(c) UU Informatica 2010"
+                           , "Over \"Schets\""
+                           , MessageBoxButtons.OK
+                           , MessageBoxIcon.Information
+                           );
+        }
+        SchetsWin s = new SchetsWin();
+        private void nieuw(object sender, EventArgs e)
+        {   
+            s.MdiParent = this;
+            s.Show();
+        }
+        private void afsluiten(object sender, EventArgs e)
+        {   this.Close();
+        }
+
+       private void opslaan(object sender, EventArgs e)
+        {
+           s.SaveAsPng();
+
+        }
+
+       private void laden(object sender, EventArgs e)
+       { }
+    }
+}
